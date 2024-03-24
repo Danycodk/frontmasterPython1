4ff2487eb17b404f6060f05a42ca1861538752ee
danycodk <danielkonate123@gmail.com>
2024-03-23 23:56:45 -0400

    finish day 8

--- env/day8/caesar-cipher.py
+++ env/day8/caesar-cipher.py
@@ -1,37 +1,30 @@
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
-# to_encrypt = False
-cipher_text = ""
-#TODO-1: Create a function called 'encrypt' that takes the 'text' and 'shift' as inputs.
-def encrypt(text,shift):
-    if direction =="encode":
-        for i in range(len(text)):
-            # print(text[i])
-          shiting_leng = alphabet.index(text[i]) + shift
-        #   print(f"what the value of the shiftting_len {shiting_leng}")
-          cipher_text = text
-        #   print(alphabet[shiting_leng])
-          cipher_text += alphabet[shiting_leng]
-        #   text[i] = alphabet[shiting_leng]
-        #   print(f"Agent here is the message {cipher_text}")
-          print(f"Look the message agent 007 {cipher_text}")
-    elif direction == "decode":
-    #    plain_text = text 
-       print("nothing")
-    #    print(f"The message real sense {plain_text}")
-    #TODO-2: Inside the 'encrypt' function, shift each letter of the 'text' forwards in the alphabet by the shift amount and print the encrypted text.  
-    #e.g. 
-    #plain_text = "hello"
-    #shift = 5
-    #cipher_text = "mjqqt"
-    #print output: "The encoded text is mjqqt"
-encrypt(text,shift=shift)
-    ##HINT: How do you get the index of an item in a list:
-    #https://stackoverflow.com/questions/176918/finding-the-index-of-an-item-in-a-list
 
-    ##🐛Bug alert: What happens if you try to encode the word 'civilization'?🐛
+#TODO-1: Combine the encrypt() and decrypt() functions into a single function called caesar(). 
 
-#TODO-3: Call the encrypt function and pass in the user inputs. You should be able to test the code and encrypt a message. 
+def encrypt(plain_text, shift_amount):
+  cipher_text = ""
+  for letter in plain_text:
+    position = alphabet.index(letter)
+    new_position = position + shift_amount
+    cipher_text += alphabet[new_position]
+  print(f"The encoded text is {cipher_text}")
+
+def decrypt(cipher_text, shift_amount):
+  plain_text = ""
+  for letter in cipher_text:
+    position = alphabet.index(letter)
+    new_position = position - shift_amount
+    plain_text += alphabet[new_position]
+  print(f"The decoded text is {plain_text}")
+
+if direction == "encode":
+  encrypt(plain_text=text, shift_amount=shift)
+elif direction == "decode":
+  decrypt(cipher_text=text, shift_amount=shift)
+
+#TODO-2: Call the caesar() function, passing over the 'text', 'shift' and 'direction' values.
--- env/day8/test.py
+++ env/day8/test.py
@@ -1,24 +1,21 @@
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+            'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 # to_encrypt = False
 cipher_text = ""
-def encrypt(text,shift):
+def encrypt(plain_text ,shift_amount):
     it_work = ""
     plain_text = "" 
     if direction =="encode":
         for i in range(len(text)):
           shiting_leng = alphabet.index(text[i]) + shift
-        #   print(cipher_text)
-        #   print(alphabet[shiting_leng])
           it_work +=  alphabet[shiting_leng]
+        
           print(it_work)
-        #   cipher_text = text
-        #   cipher_text += alphabet[shiting_leng]
 
-        #   print(f"Look the message agent 007 {cipher_text}")
     elif direction == "decode":
         plain_text = text
         print(f"The message real sense {plain_text}")
@@ -28,4 +25,4 @@ def encrypt(text,shift):
     #shift = 5
     #cipher_text = "mjqqt"
     #print output: "The encoded text is mjqqt"
-encrypt(text,shift=shift)
+encrypt(plain_text= text,shift_amount=shift)
